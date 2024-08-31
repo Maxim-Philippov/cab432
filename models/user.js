ac0c98dd4571bcd7f8deda3694d7751231324fa6
Maxim Philippov <maxim.philippov03@gmail.com>
2024-09-01 02:34:19 +1000

    updated user

--- models/user.js
+++ models/user.js
@@ -22,5 +22,6 @@ const userSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = User;
